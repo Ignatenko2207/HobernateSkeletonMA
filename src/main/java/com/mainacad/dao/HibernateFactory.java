dc33a9374b39d6bf13fc0e3cb5bbef6473f807b9
Alex <ignatenko2207@gmail.com>
2019-08-28 20:56:59 +0300

    Bye-bye xml

--- src/main/java/com/mainacad/dao/HibernateFactory.java
+++ src/main/java/com/mainacad/dao/HibernateFactory.java
@@ -1,7 +1,13 @@
 package com.mainacad.dao;
 
+import com.mainacad.model.Cart;
+import com.mainacad.model.Item;
+import com.mainacad.model.Order;
+import com.mainacad.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateFactory {
 
@@ -9,8 +15,26 @@ public class HibernateFactory {
 
     public static SessionFactory getSessionFactory(){
         if (sessionFactory == null){
-            Configuration configuration = new Configuration().configure();
-            sessionFactory = configuration.buildSessionFactory();
+            Configuration configuration = new Configuration();
+
+            configuration.setProperty("hibernate.connection.driverClassName", "org.postgresql.Driver");
+            configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/store_ma");
+            configuration.setProperty("hibernate.connection.username", "postgres");
+            configuration.setProperty("hibernate.connection.password", "248842");
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.setProperty("hibernate.format_sql", "true");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Item.class);
+            configuration.addAnnotatedClass(Cart.class);
+            configuration.addAnnotatedClass(Order.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         return sessionFactory;
     }
